65950dde1cb485dadca245d1e2271fb00d969e06
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-01-05 21:10:59 +0300

    detay.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require "simple_html_dom.php";
 $html = str_get_html(file_get_contents('https://www.sinefil.com/mini-diziler'));  // Siteye bağlan
 //$html = file_get_html('http://www.google.com/');  // Bu şekildede kullanılabilir.
 
+echo $html->find('div.detail-tag div.left-block img');
 foreach($html->find('div.movie div.col-lg-9 h3 a') as $element)  // Sitedeki tüm "a" elementlerini ara
 {
     $str=explode("/",$element->href);
