75c594a972f779f23639cc2ae39eac48b4cc5ba1
ozgurkaracam <ozgurkrcm@hotmail.com>
2019-01-08 14:03:39 +0300

    bootstrap

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+require "simple_html_dom.php";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>deneme..</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <script src="/js/bootstrap.min.js" charset="utf-8"></script>
+  </head>
+  <body>
